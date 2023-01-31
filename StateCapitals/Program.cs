3cfcb749cbc3f217aefa3ed6ddb63bba38ecfc1f
James Titus <jamesttitusjr@outlook.com>
2023-01-30 21:08:30 -0500

    Add project files.

---
+++ StateCapitals/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+Console.WriteLine("Enter a two letter state code to learn its capital.");
+string state = Console.ReadLine();
+state = state.ToUpper();
+
+string capital = "I don't know!!!!";
+
+if (state == "OH")
+{
+    capital = "Columbus";
+}
+else if (state == "KY")
+{
+    capital = "Frankfort";
+}
+else if (state == "IN")
+{
+    capital= "Indianapolis";
+}
